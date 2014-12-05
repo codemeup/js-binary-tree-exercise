d6e60802438647f046fd844d67d019d2ee1d57e9
codemeup <laurakemp@live.co.uk>
2014-12-05 10:23:19 -0800

    tree done

--- binary_tree.js
+++ binary_tree.js
@@ -5,19 +5,83 @@ function BinTree(value){
 }
 
 
-BinTree.prototype.insert = function(){
+BinTree.prototype.insert = function(newNum){
+//testing is there a node here?
+  if(this.value === null) {
+    this.value = newNum;
+  } else {
+    //if the newNum is greater calling this function to the right
+    if (newNum > this.value){
+      if (this.right === null){
+        //you can't call insert on a node that doesn't exist so we need to create a new node using new binTree(nodecontent)
+        this.right = new BinTree(newNum);
+      } else {
+        this.right.insert(newNum);
+      }
+    } else {
+    //if the newNum is greater calling this function to the right
+    if (this.left ===null){
+        //you can't call insert on a node that doesn't exist so we need to create a new node using new binTree(nodecontent)
+        this.left = new BinTree(newNum);
+      } else{
+        this.left.insert(newNum);
+      }
+    }
+    return this;
+    }
 
-};
+  };
+
+//this is refactored below to make it more efficient using a ternary:
+// BinTree.prototype.isLeaf = function(){
+//   if(this.value !== null) {
+//     if(this.right === null && this.left === null){
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } else {
+//     console.log("there's nothing in this noooode");
+//   }
+// };
+
+//refactored version of the above using a ternary:
+
 BinTree.prototype.isLeaf = function(){
-
+  if(this.value !== null) {
+    return this.right ===null && this.left ===null;
+  } else {
+    console.log("there's nothing in this noooode");
+  }
 };
-BinTree.prototype.search = function(){
 
-};
 
+BinTree.prototype.search = function(thisNum){
+  // Case 1 - this.value === thisNum
+    // return true
+  // Case 2 = this is a leaf
+    // return false
+  // Case 3 = thisNum < this.value
+    // call this.left.search(thisNum)
+  // Case 4 = don't actually need a condition here...
+    // call this.right.search(thisNum)
+  if(this.value === thisNum){
+    return true;
+  } else {
+        if(this.isLeaf() === true){
+          return false;
+        } else {
+          if(thisNum > this.value){
+            return this.right.search(thisNum);
+          } else{
+            return this.left.search(thisNum);
+          }
+        }
+      }
+  };
 
 try{
   module.exports = BinTree;
 } catch(e){
-
+//the try catch is just a way of getting the bin tree to the spec folder
 }
--- spec/binary_tree_spec.js
+++ spec/binary_tree_spec.js
@@ -1,4 +1,4 @@
-var BinTree = require("../binary_tree.js")
+var BinTree = require("../binary_tree.js");
 
 describe("BinTree", function(){
 
@@ -26,7 +26,7 @@ describe("BinTree", function(){
 
     it("set right to null", function(){
       expect(binTree.right).toBe(null);
-    })
+    });
   });
 
   describe("prototype has method", function(){
@@ -48,7 +48,7 @@ describe("BinTree", function(){
   });
 
   describe("#insert", function(){
-    var binTree
+    var binTree;
 
     beforeEach(function(){
       binTree = new BinTree(5);
@@ -69,7 +69,6 @@ describe("BinTree", function(){
     it("should return this", function(){
       expect(binTree.insert(2)).toBe(binTree);
     });
-
   });
 
   describe("#search", function(){
@@ -94,7 +93,7 @@ describe("BinTree", function(){
     var binTree = new BinTree(5);
     binTree.insert(3).insert(1).insert(2).insert(6);
       it("should check to see if a node is a leaf", function(){
-        expect(binTree.isLeaf(6)).toBe(true);
+        expect(binTree.right.isLeaf()).toBe(true);
       });
     });
 
